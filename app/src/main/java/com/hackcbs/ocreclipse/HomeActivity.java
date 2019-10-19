b22e08ca6840b244a3a35b91ef7d76a03e83e2aa
Alok <alokkumar.jnv10@gmail.com>
2019-10-20 00:18:55 +0530

    make splash screen, add fab and connected it to TakePhotoActivity

--- app/src/main/java/com/hackcbs/ocreclipse/HomeActivity.java
+++ app/src/main/java/com/hackcbs/ocreclipse/HomeActivity.java
@@ -1,13 +1,27 @@
 package com.hackcbs.ocreclipse;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.tbHome);
+    }
+
+    public void addInvoice(View view) {
+        Intent intent = new Intent(HomeActivity.this, TakePhotoActivity.class);
+        startActivity(intent);
     }
 }
